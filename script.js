b478503c9856f2456b1617dce25e08db656de6ad
Rishiyadav54 <rishi54yadav@gmail.com>
2024-04-23 11:28:08 +0530

    disclamer added

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ function loco(){
 
 loco();
 
+let mbscrn = document.querySelector("#mbscrn");
+let cross = document.querySelector("#cross");
+let main = document.querySelector("#main1");
+
+
+
 
 if(innerWidth>600){
   gsap.from(".nlinks",{
@@ -52,10 +58,13 @@ if(innerWidth>600){
     config:{"a":{"value":0.92,"range":[0,30]},"b":{"value":-0.97,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":1.6554189018796954},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":true},"growSize":{"value":2.71,"range":[1,15]},"durationOut":{"value":0.55,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":true},"maskVal":{"value":1.12,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":false},"onMouse":{"value":1},"noise_speed":{"value":0.76,"range":[0,10]},"metaball":{"value":0.11,"range":[0,2],"_gsap":{"id":32}},"discard_threshold":{"value":0.44,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.44,"range":[0,2]},"noise_scale":{"value":10.69,"range":[0,100]}},
     gooey:true,
   });
+  main.style.display = "block";
+  mbscrn.style.display = "none";
+
   
 }
 else{
-  gsap.from(".nlinks",{
+  gsap.from(".nlinks,#mbscrn h2",{
     stagger:.2,
     y:10,
     opacity:0,
@@ -63,7 +72,7 @@ else{
     ease: "power1"
   
   })
-  Shery.textAnimate("#c2-text h1", {
+  Shery.textAnimate("#c2-text h1 ,#mbscrn h1", {
     style: 2,
     y: 10,
     delay: 0.1,
@@ -72,7 +81,7 @@ else{
     multiplier: 0.1,
   });
   
-  gsap.from(".anim2",{
+  gsap.from(".anim2, #rszevi",{
     stagger:.3,
     y:50,
     opacity:0,
@@ -80,6 +89,13 @@ else{
     ease: Expo
   })
   
+  mbscrn.style.display = "flex";
+  main.style.display = "none";
+  
+  cross.addEventListener("touchstart", function(){
+    main.style.display = "block";
+    mbscrn.style.display = "none";
+  })
 }
 
 
